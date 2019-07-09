2723f055caaed348538e5282a55b7278830983b8
Daniel Moreira de Santana <daniel.qejo@gmail.com>
2019-07-09 04:45:13 -0300

    primeiro commit, criacao do projeto

---
+++ src/main/java/teste/elo7/danielsantana/sondamarte/SondaMarteApplication.java
@@ -0,0 +1,13 @@
+package teste.elo7.danielsantana.sondamarte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SondaMarteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SondaMarteApplication.class, args);
+	}
+
+}
